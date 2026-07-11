452b2044bc4d5cef2baa62be85360fdbecc30f4f
scorps <scorps@gmail.com>
2026-07-11 01:28:44 -0500

    feat: add locale strings and craft-count setting

--- settings.lua
+++ settings.lua
@@ -1,1 +1,11 @@
--- mod settings defined in Task 3
+data:extend({
+  {
+    type = "int-setting",
+    name = "ghost-crafter-craft-count",
+    setting_type = "runtime-per-player",
+    default_value = 1,
+    minimum_value = 1,
+    maximum_value = 1000,
+    order = "a",
+  }
+})
