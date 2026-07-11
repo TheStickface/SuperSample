5baad31a2e90e9c645018c323f9efe28533c8c2a
scorps <scorps@gmail.com>
2026-07-11 01:29:33 -0500

    feat: register three ghost-crafter custom inputs (base, shift, ctrl)

--- data.lua
+++ data.lua
@@ -1,1 +1,20 @@
--- custom inputs defined in Task 4
+data:extend({
+  {
+    type = "custom-input",
+    name = "ghost-crafter-craft",
+    key_sequence = "",   -- unbound by default; player assigns in Controls
+    consuming = "none",
+  },
+  {
+    type = "custom-input",
+    name = "ghost-crafter-craft-shift",
+    key_sequence = "",
+    consuming = "none",
+  },
+  {
+    type = "custom-input",
+    name = "ghost-crafter-craft-ctrl",
+    key_sequence = "",
+    consuming = "none",
+  },
+})
