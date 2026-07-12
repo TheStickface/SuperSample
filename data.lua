c05ddd371b2f38c51e0b4c02c8dd33ab81bc22a2
scorps <scorps@gmail.com>
2026-07-12 15:22:00 -0500

    fix: add required small_icon to pipette toggle shortcut prototype
    Factorio 2.0 requires small_icon/small_icon_size on shortcut prototypes;
    without it the mod failed to load with "Key small_icon not found".

--- data.lua
+++ data.lua
@@ -30,6 +30,8 @@ data:extend({
     toggleable = true,
     icon = "__base__/graphics/icons/burner-inserter.png",
     icon_size = 64,
+    small_icon = "__base__/graphics/icons/burner-inserter.png",
+    small_icon_size = 64,
     associated_control_input = "supersample-toggle-pipette",
   },
 })
